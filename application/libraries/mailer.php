e7765667aecd5adaaa3950b775a596c02b242072
= <=>
2019-11-27 18:45:02 +0700

    update

--- application/libraries/mailer.php
+++ application/libraries/mailer.php
@@ -1,6 +1,5 @@
 <?php
 class Mailer {
-
     function __construct() {
         require_once APPPATH.'third_party/PHPMailer/src/Exception.php';
         require_once APPPATH.'third_party/PHPMailer/src/PHPMailer.php';
