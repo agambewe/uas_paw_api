63932c58bf2f16d710a835e4f9a82f9271a7f4d9
= <=>
2019-11-27 22:27:51 +0700

    updt

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -4,6 +4,7 @@
 
 	require APPPATH . 'libraries/REST_Controller.php';
     require APPPATH . 'libraries/Format.php';
+
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;	
     require APPPATH."third_party/PHPMailer/src/Exception.php";
@@ -177,12 +178,12 @@
                 try {
                     $mail->SMTPDebug = 2;  
                     $mail->IsSMTP();        //Sets Mailer to send message using SMTP
-                    $mail->Host = 'smtp.gmail.com';  //Sets the SMTP hosts of your Email hosting, this for Godaddy
-                    $mail->Port = 587;        //Sets the default SMTP server port
+                    $mail->Host = 'ssl://smtp.gmail.com:465';  //Sets the SMTP hosts of your Email hosting, this for Godaddy
+                    $mail->Port = 465;        //Sets the default SMTP server port
                     $mail->SMTPAuth = true;       //Sets SMTP authentication. Utilizes the Username and Password variables
                     $mail->Username = 'iconicshoescare@gmail.com';     //Sets SMTP username
                     $mail->Password = 'IconicShoesCare!';     //Sets SMTP password
-                    $mail->SMTPSecure = 'tls';       //Sets connection prefix. Options are "", "ssl" or "tls"
+                    $mail->SMTPSecure = 'ssl';       //Sets connection prefix. Options are "", "ssl" or "tls"
                     // $mail->From = 'info@IconicGoup.gov';   //Sets the From email address for the message
                     // $mail->FromName = 'ICONIC Group';     //Sets the From name of the message
                     $mail->setFrom('iconicshoescare@gmail.com', 'ICONIC Group');
