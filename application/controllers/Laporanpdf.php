7ecc2df13408c47dd8dc564d4b28d5d33fed324d
= <=>
2019-11-27 21:12:58 +0700

    updt

--- application/controllers/Laporanpdf.php
+++ application/controllers/Laporanpdf.php
@@ -5,46 +5,47 @@ Class Laporanpdf extends CI_Controller{
         parent::__construct();
         $this->load->library('Pdf');
         $this->load->model("PotongModel"); 
+        $this->load->model("MakeupModel"); 
     }
-    function index(){
+    
+    function potong_get(){
         $pdf = new FPDF('l','mm','A4');
-// Menambah halaman baru
-$pdf->AddPage();
-// Setting jenis font
-$pdf->SetFont('Arial','B',16);
-// Membuat string
-$pdf->Cell(260,7,'Jadwal Kuliah Mahasiswa',0,1,'C');
-// Setting spasi kebawah supaya tidak rapat
-$pdf->Cell(10,7,'',0,1);
+        // Menambah halaman baru
+        $pdf->AddPage();
+        // Setting jenis font
+        $pdf->SetFont('Arial','B',16);
+        // Membuat string
+        $pdf->Cell(260,7,'Jadwal Kuliah Mahasiswa',0,1,'C');
+        // Setting spasi kebawah supaya tidak rapat
+        $pdf->Cell(10,7,'',0,1);
 
-$pdf->SetFont('Arial','B',9);
-$pdf->Cell(10,6,'NO',1,0);
-$pdf->Cell(43,6,'EMAIL',1,0);
-$pdf->Cell(40,6,'NO TELEPON',1,0);
-$pdf->Cell(27,6,'MODEL',1,0);
-$pdf->Cell(21,6,'WARNA',1,0);
-$pdf->Cell(30,6,'TANGGAL',1,0);
-$pdf->Cell(20,6,'JAM',1,0);
-$pdf->Cell(22,6,'PEMOTONG',1,0);
-$pdf->Cell(30,6,'PAKET',1,1);
+        $pdf->SetFont('Arial','B',9);
+        $pdf->Cell(10,6,'NO',1,0);
+        $pdf->Cell(43,6,'EMAIL',1,0);
+        $pdf->Cell(40,6,'NO TELEPON',1,0);
+        $pdf->Cell(27,6,'MODEL',1,0);
+        $pdf->Cell(21,6,'WARNA',1,0);
+        $pdf->Cell(30,6,'TANGGAL',1,0);
+        $pdf->Cell(20,6,'JAM',1,0);
+        $pdf->Cell(22,6,'PEMOTONG',1,0);
+        $pdf->Cell(30,6,'PAKET',1,1);
 
-$pdf->SetFont('Arial','',9);
-$query = $this->PotongModel->getTerlayaniPotong();
-$no = 1;
-foreach( $query as $row )
-{
-    $pdf->Cell(10,6,$no,1,0);
-    $pdf->Cell(43,6,$row->email,1,0);
-    $pdf->Cell(40,6,$row->noTelepon,1,0);
-    $pdf->Cell(27,6,$row->modelRambut,1,0);
-    $pdf->Cell(21,6,$row->warna,1,0);
-    $pdf->Cell(30,6,$row->tanggal,1,0);
-    $pdf->Cell(20,6,$row->jam,1,0);
-    $pdf->Cell(22,6,$row->pemotong,1,0);
-    $pdf->Cell(30,6,$row->paket,1,1);
-    $no++;
-}
-
-$pdf->Output();
+        $pdf->SetFont('Arial','',9);
+        $query = $this->PotongModel->getTerlayaniPotong();
+        $no = 1;
+        foreach( $query as $row )
+        {
+            $pdf->Cell(10,6,$no,1,0);
+            $pdf->Cell(43,6,$row->email,1,0);
+            $pdf->Cell(40,6,$row->noTelepon,1,0);
+            $pdf->Cell(27,6,$row->modelRambut,1,0);
+            $pdf->Cell(21,6,$row->warna,1,0);
+            $pdf->Cell(30,6,$row->tanggal,1,0);
+            $pdf->Cell(20,6,$row->jam,1,0);
+            $pdf->Cell(22,6,$row->pemotong,1,0);
+            $pdf->Cell(30,6,$row->paket,1,1);
+            $no++;
+        }
+    $pdf->Output();
     }
 }
