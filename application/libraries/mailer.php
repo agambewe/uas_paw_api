481da43bbaf8af7ad0c9887cc89de594a9832c6d
= <=>
2019-11-27 21:47:21 +0700

    updt

--- application/libraries/mailer.php
+++ application/libraries/mailer.php
@@ -1,9 +1,24 @@
 <?php
-class Mailer {
-    function __construct() {
-        require_once APPPATH.'third_party/PHPMailer/src/Exception.php';
-        require_once APPPATH.'third_party/PHPMailer/src/PHPMailer.php';
-        require_once APPPATH.'third_party/PHPMailer/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class Mailer
+{
+    public function __construct()
+    {
+        log_message('Debug', 'PHPMailer class is loaded.');
+    }
+
+    public function load()
+    {
+		require_once APPPATH."third_party/PHPMailer/Exception.php";
+        require_once APPPATH."third_party/PHPMailer/PHPMailer.php";
+		require_once APPPATH."third_party/PHPMailer/SMTP.php";
+
+        $objMail = new PHPMailer;
+        return $objMail;
     }
 }
+
 ?>
